e4c25a9ed80a6396e58b7c97f5e020e9768e21eb
ehdgus6876 <40512570+ehdgus6876@users.noreply.github.com>
2020-09-24 17:00:07 +0900

    계좌 QR코드 생성 기능 구현

--- express_server.js
+++ express_server.js
@@ -73,6 +73,10 @@ app.get("/balance", function(req,res){
   res.render("balance");
 })
 
+app.get('/qrcode',function(req,res){
+  res.render("qrcode");
+})
+
 app.get("/authResult",function(req,res){
   var authCode = req.query.code;
   
